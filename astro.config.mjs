08e89de34187f110636fba7cce308b9fb3d3f899
Jeremias <jeregonza194@gmail.com>
2026-05-19 13:40:57 -0300

    Finalizo mi pagina hecho en astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwindcss from '@tailwindcss/vite';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Configurás el sitio base (esencial para que funcione tu paquete RSS)
+  // aquí va la url del sitio
+  site: "https://example.com",
+
+  // 2. Configurás el plugin de Tailwind CSS a través de Vite
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [preact()]
 });
